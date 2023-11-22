f7b17b19f8dc2a3931a6455bdf804ade26a9198f
Szymon Sobczak <szymon@kanrisoft.eu>
2023-11-22 01:09:36 +0100

    add settings draft

--- app/forms/user_forms.py
+++ app/forms/user_forms.py
@@ -6,7 +6,6 @@ from datetime import datetime, timedelta
 
 
 class LoginForm(FlaskForm):
-
     username = StringField('Username', validators=[
                            validators.InputRequired()], render_kw={"placeholder": "Username"})
 
@@ -16,9 +15,28 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Login")
 
 
+class PasswordUpdateForm(FlaskForm):
+    new_password = PasswordField('New Password', validators=[
+        validators.InputRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')],
+        render_kw={"placeholder": "Password"})
+    confirm = PasswordField('Repeat New password', validators=[validators.InputRequired()], render_kw={
+                            "placeholder": "Repeat new password"})
+
+    password = PasswordField('Current password', validators=[validators.InputRequired()], render_kw={
+                             "placeholder": "Password"})
+
+    submit = SubmitField("Update password")
+
+
 class DateTimeForm(FlaskForm):
     startdate = DateField('Start Date', format='%Y-%m-%d',
                           validators=[validators.DataRequired()], default=(datetime.now() - timedelta(days=1)).date())
     enddate = DateField('End Date', format='%Y-%m-%d',
                         validators=[validators.DataRequired()], default=datetime.now().date())
     submit = SubmitField('Submit')
+
+    def validate_enddate(form, field):
+        if field.data <= form.startdate.data:
+            raise validators.ValidationError(
+                'End Date must be greater than Start Date')
--- app/routers/employee.py
+++ app/routers/employee.py
@@ -1,12 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from io import BytesIO
 import json
 
 from flask import Blueprint
 from flask import current_app as app
-from flask import flash, redirect, render_template, request, session, url_for, send_file
+from flask import flash, redirect, render_template, request, send_file, session, url_for
 from flask_login import current_user, login_required, login_user, logout_user, UserMixin
-from forms.user_forms import LoginForm, DateTimeForm
+from forms.user_forms import DateTimeForm, LoginForm, PasswordUpdateForm
 import qrcode
 import requests
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -44,18 +44,69 @@ def orders():
     return render_template("employee/orders.html", orders=orders)
 
 
-@employee_bp.route('/timelog/')
+@employee_bp.route('/admin/employees/new_password', methods=['GET', 'POST'])
+@login_required
+def settings():
+    backend_uri = app.config['BACKEND_URI']
+
+    password_form = PasswordUpdateForm()
+    if password_form.validate_on_submit():
+        if password_form.password.data == current_user.user_json.get("password"):
+            user_id = current_user.user_json.get("user_id")
+            request = requests.put(
+                url=f'{backend_uri}/api/users/{user_id}',
+                params={"user_name": current_user.user_json.get("user_name"),
+                        "password": password_form.new_password.data})
+            if request.status_code != 200:
+                flash(f"Error: {request.text}")
+            else:
+                flash("Password updated successfully!")
+                redirect(url_for('employee_bp.settings'))
+        else:
+            flash("Current password is incorrect.")
+
+    return render_template('employee/settings.html', form=password_form)
+
+
+@employee_bp.route('/timelog/', methods=['GET', 'POST'])
 @login_required
 def timelog():
     form = DateTimeForm()
+    backend_uri = app.config['BACKEND_URI']
+    user_id = current_user.user_json.get("user_id")
 
+    start_date = ((datetime.now()-timedelta(days=1))
+                  .date()
+                  .strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
+    end_date = (datetime.combine(datetime.now().date(), datetime.max.time())
+                .strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
+
+    response = requests.get(f"{backend_uri}/api/production_log/list/timeseries/{user_id}",
+                            params={"min_timestamp": start_date,
+                                    "max_timestamp": end_date})
+    timelog = json.loads(response.text)
+    for entry in timelog:
+        entry["creation_date"] = datetime.strptime(
+            entry["creation_date"][:-7], "%Y-%m-%dT%H:%M:%S")
     if form.validate_on_submit():
-        # Handle form submission here
-        selected_time_interval = form.time_interval_picker.data
-        print(selected_time_interval)
-        # Perform further processing with the selected time interval
+        try:
+            start_date = (form.startdate.data
+                          .strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
+            end_date = (form.enddate.data
+                        .strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
 
-    return render_template('employee/timelog.html', form=form)
+            response = requests.get(f"{backend_uri}/api/production_log/list/timeseries/{user_id}",
+                                    params={"min_timestamp": start_date,
+                                            "max_timestamp": end_date})
+            timelog = json.loads(response.text)
+            for entry in timelog:
+                entry["creation_date"] = datetime.strptime(
+                    entry["creation_date"][:-7], "%Y-%m-%dT%H:%M:%S")
+        except Exception as e:
+            flash(e)
+        return render_template('employee/timelog.html', form=form, timelog=timelog)
+
+    return render_template('employee/timelog.html', form=form, timelog=timelog)
 
 
 @employee_bp.route('/generate_qrcode/<order_name>')
